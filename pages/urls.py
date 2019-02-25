ef5ac936d550553179081caab6bc0d2ee32bd892
Jack Schefer <jack.schefer9@gmail.com>
2019-02-25 17:26:19 -0500

    add a profile update page and form

--- pages/urls.py
+++ pages/urls.py
@@ -8,7 +8,8 @@ app_name = 'pages'
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
     url('login', views.LoginPageView.as_view(), name='login'),
-    url('profile', login_required(views.ProfilePageView.as_view()), name='profile'),
+    url(r'profile$', login_required(views.ProfilePageView.as_view()), name='profile'),
+    url(r'profile/edit$', login_required(views.ProfileEditView.as_view()), name='edit_profile'),
     url('feed', login_required(views.FeedPageView.as_view()), name='feed'),
-    url('availability', login_required(views.AvailabilityPageView.as_view()), name='availability')
+    url('availability', login_required(views.AvailabilityPageView.as_view()), name='availability'),
 ]
