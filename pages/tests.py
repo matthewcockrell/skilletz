4f84a825b3b595be44f04fc51e5dbfea01e218ff
Marina Kun <kunmarina@gmail.com>
2019-03-04 00:34:23 -0500

    add beginning of test case for filter

--- pages/tests.py
+++ pages/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from django.test import Client, TestCase
 from django.contrib.auth.models import User
+from login.models import Profile
 
 # Create your tests here.
-#def test_feed_populate(self)
-    #client = Client()
-    #client.force_login(User.objects.get_or_create(username='testuser')[0])
-    #response = client.get('/feed')
-    #filter = Post()
+class TestViews(TestCase):
+    def test_feed_populate(self):
+        client = Client()
+        response = client.get('/')
+        client.force_login(User.objects.get_or_create(username='testuser')[0])
+        profile_list = Profile.objects.all()
+        request = client.get('/feed/',{'first_name':'Marina', 'last_name':'Kun'})
+        self.assertEqual(len(filter.qs),1)
