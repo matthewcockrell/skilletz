a997f784d5d36c96d4f7c8560a9f085d493640f0
Jack Schefer <jack.schefer9@gmail.com>
2019-03-04 10:05:56 -0500

    Added some tests for login functionality

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import Client, TestCase, RequestFactory
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from login import views
+
+class TestLogin(TestCase):
+    # Helper method
+    def _login_and_create_profile(client):
+        pass
+
+    # T1: On login, user is redirected to google
+    def test_google_oath_redirection(self):
+        client = Client()
+        response = client.get(reverse('pages:feed'))
+        self.assertRedirects(response, '/oauth/login/google-oauth2/?next=/feed', fetch_redirect_response=False)
+
+    # T7: A new user gets redirected to the profile creation page
+    def test_redirection_to_profile_creation(self):
+        fact = RequestFactory()
+        req = fact.get(reverse('pages:feed'))
+        req.user = User.objects.get_or_create(username='testuser1')[0]
+        response = views.new_user(req)
+        self.assertInHTML('Profile Creation', str(response.content))
+
+    def test_redirection_to_feed_for_initialized_profile(self):
+        fact = RequestFactory()
+        req = fact.get(reverse('pages:feed'))
+        req.user = User.objects.get_or_create(username='testuser1')[0]
+
+        req.user.profile.first_name = 'Jimbo'
+        req.user.profile.last_name = 'Jambo'
+        req.user.profile.computing_id = 'abc3de'
+        req.user.profile.save()
+
+        response = views.new_user(req)
+        self.assertRedirects(response, reverse('pages:feed'), fetch_redirect_response=False)
