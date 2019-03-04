1ef1731f12d97651bf5d20d23d4535ff676a6919
Marina Kun <kunmarina@gmail.com>
2019-03-04 00:04:07 -0500

    add slash to media_url in settings.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+#def test_feed_populate(self)
+    #client = Client()
+    #client.force_login(User.objects.get_or_create(username='testuser')[0])
+    #response = client.get('/feed')
+    #filter = Post()
