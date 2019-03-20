e04713ea4f3d3137ed710df4b0fa920b1e926e45
Elizabeth <eh7bu@virginia.edu>
2019-03-19 22:44:11 -0400

    Revert "profile pic and resume"
    This reverts commit 6321ddb554c10de772f7dd236e9b62a0e47b55cd.

--- login/forms.py
+++ login/forms.py
@@ -2,14 +2,7 @@ from django.forms import ModelForm
 
 from .models import Profile
 
-
 class NewProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'graduation_year', 'major', 'computing_id']
-
-
-class EditProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['profile_pic', 'first_name', 'last_name', 'graduation_year', 'major', 'computing_id', 'resume']
--- login/views.py
+++ login/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import NewProfileForm, EditProfileForm
+from .forms import NewProfileForm
 from .models import Profile
 
 from django.urls import reverse
@@ -10,15 +10,17 @@ from django.contrib.auth import logout
 
 def new_user(request):
     if request.method == 'POST':
-        form = NewProfileForm(request.POST, request.FILES)
+        form = NewProfileForm(request.POST)
         if form.is_valid():
             profile_to_save = form.save(commit=False) # don't commit yet so we can add the user
 
+            #request.user.profile.profile_pic = profile_to_save.profile_pic
             request.user.profile.first_name = profile_to_save.first_name
             request.user.profile.last_name = profile_to_save.last_name
             request.user.profile.graduation_year = profile_to_save.graduation_year
             request.user.profile.major = profile_to_save.major
             request.user.profile.computing_id = profile_to_save.computing_id
+            #request.user.profile.resume = profile_to_save.resume
 
             request.user.profile.save()
 
@@ -32,25 +34,6 @@ def new_user(request):
 
     return render(request, 'login/new_user.html', {'form': form})
 
-def edit_user(request):
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile_to_save = form.save(commit=False)
-
-            request.user.profile.profile_pic = profile_to_save.profile_pic
-            request.user.profile.first_name = profile_to_save.first_name
-            request.user.profile.last_name = profile_to_save.last_name
-            request.user.profile.graduation_year = profile_to_save.graduation_year
-            request.user.profile.major = profile_to_save.major
-            request.user.profile.computing_id = profile_to_save.computing_id
-            request.user.profile.resume = profile_to_save.resume
-
-            request.user.profile.save()
-
-            return redirect(reverse('pages/profile.html'))
-
-    return render(request, 'login/profile_update_form.html', {'form' : form})
 
 def logout_user(request):
     logout(request)
