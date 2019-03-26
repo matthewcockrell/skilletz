ffe17ee1b884bff21ed30be79d4b82ffd4ad221a
Elizabeth <eh7bu@virginia.edu>
2019-03-26 16:37:27 -0400

    profile pic and resume

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from . import views
@@ -10,4 +12,4 @@ urlpatterns = [
     url(r'new_user', views.new_user, name='new_user'),
     url(r'logout', views.logout_user, name='logout'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
