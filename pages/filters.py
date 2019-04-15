06c3a941db5e555b615da917f05e615e2bfb82ea
Marina Kun <mk3qa@virginia.edu>
2019-04-15 18:03:38 -0400

    change case sensitivity for filters

--- pages/filters.py
+++ pages/filters.py
@@ -3,6 +3,9 @@ from login.models import Profile
 import django_filters
 
 class ProfileFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(label='First Name:', lookup_expr='icontains')
+    last_name = django_filters.CharFilter(label = 'Last Name:', lookup_expr='icontains')
+    computing_id = django_filters.CharFilter(label = 'Computing ID:', lookup_expr='icontains')
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'computing_id', 'major', 'availability', 'courses']
