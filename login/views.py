9252001178a041f99ab1972cf991d26b609d088b
Jack Schefer <jack.schefer9@gmail.com>
2019-04-24 00:16:07 -0400

    a few polishes: 1. make the email at the top of profile a mailto link 2. allow non-UVA emails but add verification 3. add login error page 4. fix styling of boxes on the profile page

--- login/views.py
+++ login/views.py
@@ -7,25 +7,39 @@ from django.urls import reverse
 from django.shortcuts import redirect
 from django.conf import settings
 from django.contrib.auth import logout
+from django.views.generic import TemplateView
+
+SHERRIFF_EMAIL = 'sherriff@gmail.com'
+SHERRIFF_COMPUTING_ID = 'mss2x'
 
 def new_user(request):
     if request.method == 'POST':
         form = NewProfileForm(request.POST)
         if form.is_valid():
             profile_to_save = form.save(commit=False) # don't commit yet so we can add the user
+            
+            form_email = request.user.email
+            if '@virginia.edu' in form_email or form_email == SHERRIFF_EMAIL:
+                #request.user.profile.profile_pic = profile_to_save.profile_pic
+                request.user.profile.first_name = profile_to_save.first_name
+                request.user.profile.last_name = profile_to_save.last_name
+                request.user.profile.graduation_year = profile_to_save.graduation_year
+                request.user.profile.major = profile_to_save.major
+                #request.user.profile.resume = profile_to_save.resume
 
-            #request.user.profile.profile_pic = profile_to_save.profile_pic
-            request.user.profile.first_name = profile_to_save.first_name
-            request.user.profile.last_name = profile_to_save.last_name
-            request.user.profile.graduation_year = profile_to_save.graduation_year
-            request.user.profile.major = profile_to_save.major
-            request.user.profile.computing_id = profile_to_save.computing_id
-            #request.user.profile.resume = profile_to_save.resume
+                if form_email == SHERRIFF_EMAIL:
+                    request.user.profile.computing_id = SHERRIFF_COMPUTING_ID
+                else:
+                    at_symbol_index = form_email.find('@')
+                    request.user.profile.computing_id = form_email[:at_symbol_index]
 
-            request.user.profile.save()
+                request.user.profile.save()
 
-            return redirect(reverse(settings.POST_LOGIN_HOME_URL))
+                return redirect(reverse(settings.POST_LOGIN_HOME_URL))
 
+    elif not request.user or ('@virginia.edu' not in request.user.email and request.user.email != SHERRIFF_EMAIL):
+        logout(request)
+        return redirect(reverse('login:error'))
     elif request.user.profile and request.user.profile.has_been_initialized():
         return redirect(reverse(settings.POST_LOGIN_HOME_URL))
 
@@ -34,6 +48,8 @@ def new_user(request):
 
     return render(request, 'login/new_user.html', {'form': form})
 
+class ErrorView(TemplateView):
+    template_name = 'login/error.html'
 
 def logout_user(request):
     logout(request)
