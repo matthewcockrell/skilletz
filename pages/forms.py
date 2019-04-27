49c940a3fff0902bd404f9fa776708cb1f450472
Jack Schefer <jack.schefer9@gmail.com>
2019-04-27 17:47:47 -0400

    make bio, availability, and courses optional fields

--- pages/forms.py
+++ pages/forms.py
@@ -6,3 +6,9 @@ class ProfileEditForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'graduation_year', 'major', 'courses', 'availability', 'bio']
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileEditForm, self).__init__(*args, **kwargs)
+        self.fields['courses'].required = False
+        self.fields['availability'].required = False
+        self.fields['bio'].required = False
